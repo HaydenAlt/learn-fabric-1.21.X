c49dc226a7c201b86c287c5a14d027e76875922f
HaydenAlt <s2210132@student.hpccss.edu.hk>
2026-03-30 21:35:34 +0800

    Initial commit

---
+++ src/main/java/com/haydenalt/learn/Learn.java
@@ -0,0 +1,15 @@
+package com.haydenalt.learn;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Learn implements ModInitializer {
+	public static final String MOD_ID = "learn";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/haydenalt/learn/LearnClient.java
@@ -0,0 +1,10 @@
+package com.haydenalt.learn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
